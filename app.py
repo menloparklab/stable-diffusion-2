de69534b0be75bb6a953d26250ca63baad0848a1
misbahsy <35427888+misbahsy@users.noreply.github.com>
2022-11-28 07:42:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,8 @@ def inference(model_inputs:dict) -> dict:
     num_inference_steps = model_inputs.get('num_inference_steps', 50)
     guidance_scale = model_inputs.get('guidance_scale', 7.5)
     input_seed = model_inputs.get("seed",None)
+    negative_prompt = model_inputs.get('negative_prompt', None)
+    
     
     #If "seed" is not sent, we won't specify a seed in the call
     generator = None
@@ -40,7 +42,7 @@ def inference(model_inputs:dict) -> dict:
     
     # Run the model
     with autocast("cuda"):
-        image = model(prompt,height=height,width=width,num_inference_steps=num_inference_steps,guidance_scale=guidance_scale,generator=generator).images[0]
+        image = model(prompt,height=height,width=width,num_inference_steps=num_inference_steps,guidance_scale=guidance_scale,generator=generator, negative_prompt=negative_prompt).images[0]
     
     buffered = BytesIO()
     image.save(buffered,format="JPEG")
